d139b1694d9290efd6c36e92c86fb1d5c5021f33
Sebastian Paarmann <sebastian.paarmann@gmail.com>
2018-05-29 15:09:40 +0200

    Remove explicit type specifiers from tests

--- Math_tests.hs
+++ Math_tests.hs
@@ -21,11 +21,11 @@ tests = [
   ]
 
 prop_add_comm x y = (add x y) == (add y x)
-  where types = (x :: Rational, y :: Rational)
+--  where types = (x :: Rational, y :: Rational)
 prop_mult_comm x y = (mult x y) == (mult y x)
-  where types = (x :: Rational, y :: Rational)
+--  where types = (x :: Rational, y :: Rational)
 
 prop_add_sub_inverse x y = (sub (add x y) y) == x
-  where types = (x :: Rational, y :: Rational)
+--  where types = (x :: Rational, y :: Rational)
 prop_mult_divide_inverse x y = y /= 0 ==> (mult (divide x y) y) == x
-  where types = (x :: Rational, y :: Rational)
+--  where types = (x :: Rational, y :: Rational)
